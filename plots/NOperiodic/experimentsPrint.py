9a3549d7fc4f1052e8ee1f0680030e9895bc9cd7
Ignacio <ignacio.eguinoa@gmail.com>
2015-05-10 02:51:47 -0300

    avances muchos :)

--- plots/NOperiodic/experimentsPrint.py
+++ plots/NOperiodic/experimentsPrint.py
@@ -8,22 +8,27 @@ from graphs import *
 
 
 
+
+#COMPARACION PARA SISTEMA DE 8K PARTICULAS 
+  
 def comparacionTiempos1Niteraciones():
   
 #**** PRIMERA COMPARACION PARA periodico
- 
-  gpu1_periodic=522.000
-  gpuPot1_periodic=837.903
-  gpuDer1_periodic=1000.000
+   
   
-  gpuN_periodic=522.000
-  gpuPotN_periodic=837.903
-  gpuDerN_periodic=1000.000
+   #310ms
+  gpu1_periodic=310.110
+  gpuPot1_periodic=237.903
+  gpuDer1_periodic=210.012
+  
+  gpuN_periodic=322.000
+  gpuPotN_periodic=227.903
+  gpuDerN_periodic=191.080
  
 
   
 
-  labels = [u"1 iteración", u"N iteraciones"]
+  labels = [u"Iteración 1", u"Iteración 1000"]
   comparison = {
       u"GPU Base": [gpu1_periodic, gpuN_periodic ],
       u"GPU Tabla Potenciales": [gpuPot1_periodic, gpuPotN_periodic ],
@@ -34,7 +39,7 @@ def comparacionTiempos1Niteraciones():
       'values': comparison,
       'ticks': labels,
       'filename': u'comparacion-periodico.png',
-      'ylabel': u'Tiempo promedio por iteración [ms]',
+      'ylabel': u'Tiempo por iteración [ms]',
       'loc': 'left'
   }
 
@@ -42,23 +47,23 @@ def comparacionTiempos1Niteraciones():
   
 
 #AHORA HACER UNA COMPARACION PARA CONDICIONES NO PERIODICAS
-   
+#QUE VALOR DE CUTOFF????   
  
-  gpu1_NOper=704.000
-  gpuPot1_NOper=1019.784
-  gpuDer1_NOper=1000.000
+  gpu1_NOper=300.104
+  gpuPot1_NOper=227.784
+  gpuDer1_NOper=205.000
 
-  gpuN_NOper=704.000
-  gpuPotN_NOper=1019.784
-  gpuDerN_NOper=1000.000
+  gpuN_NOper=250.000
+  gpuPotN_NOper=190.784
+  gpuDerN_NOper=178.000
 
   
 
-  labels = [u"1 iteración", u"N iteraciones"]
+  labels = [u"Iteración 1", u"Iteración 1000"]
   comparison = {
       u"GPU Analítico": [gpu1_NOper, gpuN_NOper ],
       u"GPU Tabla Potenciales": [gpuPot1_NOper, gpuPotN_NOper ],
-      u'GPU Tabla Derivadas': [gpuDer1_NOper,gpuDer1_NOper ],
+      u'GPU Tabla Derivadas': [gpuDer1_NOper,gpuDerN_NOper ],
   }
 
   params = {
